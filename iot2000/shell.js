1f3a7eddb1ac45c15c6d120c0e9b872681c6aac5
tchapi <regbasket+github@gmail.com>
2018-04-11 08:14:52 +0200

    Refactor shell, add config file

--- iot2000/shell.js
+++ iot2000/shell.js
@@ -1,47 +1,50 @@
-const MIRROR_SERVER ='http://mirror.predictable.run:3000' ;
+const MIRROR_SERVER = 'http://mirror.predictable.run:3000'
 
-var shell = require('/usr/lib/node_modules/socket.io-client')(MIRROR_SERVER);
+const shell = require('/usr/lib/node_modules/socket.io-client')(MIRROR_SERVER)
+const logger = require('./services/logger')
 
-var os = require("os");
+const os = require('os')
+const exec = require('child_process').exec
 
-var exec = require('child_process').exec;
+logger.init('shell')
 
-shell.on('connect', function() {
-    shell.emit("get_port", os.hostname());
-});
+shell.on('connect', () => {
+  logger.log('Connected, emiting "get_port"')
+  shell.emit('get_port', os.hostname())
+})
 
-shell.on('disconnect', function() {});
+shell.on('disconnect', () => {
+  logger.log('Disconnected')
+})
 
-shell.on('open_tunnel', function (port) {
-    //exec('ssh -R 0.0.0.0:'+port+'+:0.0.0.0:22 ubuntu@mirror.predictable.run -i /root/id_rsa -y -N');
-    console.log(os.arch());
-    if(os.arch() == 'ia32')  
-    {  
-        console.log("openssh on remote server port "+port);
-        //exec('kill -9 $(pgrep -x ssh); sleep 1');
-        exec('if pgrep -x ssh; then echo "already running"; else ssh -i /home/root/predictablefarm.shellmirror.pem -Ng -R '+port+':127.0.0.1:22 ubuntu@mirror.predictable.run  ; fi '); //openssh on yocto
-    } 
-    else 
-    {   
-        console.log("dropbear on remote server port "+port);
-        exec('kill -9 $(pgrep -l ssh)');
-        exec('ssh -R 0.0.0.0:'+port+'+:0.0.0.0:22 ubuntu@mirror.predictable.run -i /root/id_rsa -y -N'); //dropbear on linino   
-    }
-    shell.emit("tunnel_ok", os.hostname());
-});
+shell.on('open_tunnel', (port) => {
+  if (os.arch() == 'ia32') {
+    // On YOCTO, it's OpenSSH
+    console.log('OpenSSH on remote server port %d', port)
+    // exec('kill -9 $(pgrep -x ssh); sleep 1');
+    exec('if pgrep -x ssh; then echo "already running"; else ssh -i /home/root/predictablefarm.shellmirror.pem -N -R ' + port + ':localhost:22 ubuntu@mirror.predictable.run; fi ')
+  } else {
+    // On Linino, it's DropBear
+    console.log('Dropbear on remote server port %d', port)
+    exec('kill -9 $(pgrep -l ssh)')
+    exec('ssh -R 0.0.0.0:' + port + '+:0.0.0.0:22 ubuntu@mirror.predictable.run -i /root/id_rsa -y -N') // dropbear on linino
+  }
+  shell.emit('tunnel_ok', os.hostname())
+})
+
 /*
-shell.on('reset', function () {
-    exec('reset-mcu');
-    shell.emit("reset-mcu_ok", os.hostname());
-});
+shell.on('reset', () => {
+  exec('reset-mcu')
+  shell.emit('reset-mcu_ok', os.hostname())
+})
 
-shell.on('reboot', function () {
-    shell.emit("reboot_ok", os.hostname());
-    exec('reboot');
-});
+shell.on('reboot', () => {
+  shell.emit('reboot_ok', os.hostname())
+  exec('reboot')
+})
 
-shell.on('update', function () {
-    shell.emit("update_ok", os.hostname());
-    exec('/etc/init.d/bridge update');
-});
+shell.on('update', () => {
+  shell.emit('update_ok', os.hostname())
+  exec('/etc/init.d/bridge update')
+})
 */
