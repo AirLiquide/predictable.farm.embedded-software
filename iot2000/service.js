2ffa35728f3f685732bc7b9c4e949ae378f66d0f
tchapi <regbasket+github@gmail.com>
2018-04-11 12:01:45 +0200

    Add local engine management

--- iot2000/service.js
+++ iot2000/service.js
@@ -4,6 +4,7 @@ var DEVICE_ID = 0
 
 var SERVER_URL = 'http://lafactory.predictable.zone'
 const UPDATE_ENDPOINT = 'http://update.predictablefarm.net/'
+const GRAPH_FILE = __dirname + '/graphs/graph.json'
 
 const config = require('./config/default')
 const logger = require('./services/logger')
@@ -15,10 +16,8 @@ process.argv.forEach(function (val, index, array) {
   if (index >= 2) {
     if (index == 2) {
       DEVICE_ID = val
-      logger.log('Device ID is ' + DEVICE_ID)
     } else if (index == 3) {
       SERVER_URL = 'http://' + val + '.predictable.zone'
-      logger.log('Server URL is ' + SERVER_URL)
     }
   }
 })
@@ -28,12 +27,24 @@ process.argv.forEach(function (val, index, array) {
 // const MutexPromise = require('mutex-promise');
 const exec = require('child_process').exec
 const https = require('https')
+const fs = require('fs')
 
 const client = require('/usr/lib/node_modules/socket.io-client')
 const server = require('/usr/lib/node_modules/socket.io')
 
 const MODE_LOCAL = 0
 const MODE_REMOTE = 1
+
+// Local config parameters
+var local_sockets = [] // Local sockets for client
+
+// Do we have a local graph ?
+var graph_file = null
+var graph = null
+try {
+  graph_file = fs.readFileSync(GRAPH_FILE, {encoding: 'utf-8'})
+  graph = JSON.parse(graph_file)
+} catch (err) {}
 
 var networkStatus = {
   remote_socket: false,
@@ -54,6 +65,9 @@ var relay4_mode = '0'
 var last_server_command = ''
 
 logger.log('Starting app v' + config.version + '...')
+logger.log('▸ Device ID: ' + DEVICE_ID)
+logger.log('▸ Server URL: ' + SERVER_URL)
+
 // var mutex = new MutexPromise('sync_write');
 
 logger.log('Starting app ...')
@@ -61,7 +75,71 @@ logger.log('Starting app ...')
 logger.log(' 1. Opening local io socket ')
 var my_io = server(config.socket.port)
 
-logger.log(' 2. Opening remote socket to ' + SERVER_URL)
+// Do we have a local graph ?
+// if Yes, parse and create the necessary stuff
+if (graph) {
+  logger.log(' 2. Found a local graph file, enabling local sockets')
+
+  // Client to the local engine socket
+  var engine_socket = client('http://localhost:' + config.local.engine.port)
+
+  // If the engine starts late
+  engine_socket.on('get-config', function () {
+    // Give everything to the local engine so it can init itself
+    engine_socket.emit('set-config', {device_id: DEVICE_ID, graph: graph})
+  })
+
+  engine_socket.on('connect', function () {
+    logger.log('** Connected to local engine socket on port: ' + config.local.engine.port)
+  })
+  engine_socket.on('connect_error', function (error) {
+    logger.log('** Connection error to local engine socket: ' + error)
+  })
+  engine_socket.on('config-ok', function (data) {
+    if (data.type == 'server') {
+      logger.log(' * This bridge is a LOCAL SERVER (mixed)')
+      // We are an actuator, create a server
+      logger.log(' * Creating local server on ' + config.local.server.port)
+      local_server = server(config.local.server.port)
+      local_server.on('connect', function (local_socket) {
+        logger.log(' * Local engine is connected')
+        // The lib will create a client connection to the local_server, namely
+        // this url : localhost:config.local.server.port - it does not need any info
+
+        // This will be sent by the sensors, locally on the same network
+        local_socket.on('sensor-emit', function (data) {
+          // Here we are gonna receive raw messages from local sensors
+          // Let's push that to the local engine
+          engine_socket.emit('sensor-emit', data)
+        })
+      })
+    } else {
+      logger.log(' * This bridge is a LOCAL CLIENT')
+    }
+
+    // We are a sensor (or a server), create as many sockets as the local engine tells us
+    // so we can send data to the devices that have the relays (possibly, ourself)
+    logger.log('** Opening local sockets :')
+
+    data.relays.forEach(function (server) {
+      var address = 'http://' + ((server.deviceid == DEVICE_ID) ? 'localhost' : ('predictable' + server.deviceid + '.local'))
+      logger.log(' * --> ' + address + ':' + config.local.server.port)
+      local_sockets.push(client(address + ':' + config.local.server.port))
+    })
+    sendNetworkStatus()
+  })
+
+  // This will be sent by the engine, locally on the same node
+  engine_socket.on('sensor-receive', function (data) {
+    // Here we are gonna receive messages from the engine, in the
+    // same grammar than the cloud ones
+    processIncomingCommand(JSON.stringify(data), MODE_LOCAL)
+  })
+} else {
+  logger.log(' 2. No local graph found, skipping local socket configuration')
+}
+
+logger.log(' 3. Opening remote socket to ' + SERVER_URL)
 
 utils.led(utils.colors.RED)
 
@@ -77,7 +155,7 @@ var remote_socket = client(
   }
 )
 
-logger.log(' 3. Starting network check')
+logger.log(' 4. Starting network check')
 var networkCheck = setInterval(function () {
   // var test_file = 'http://update.predictablefarm.net/' + DEVICE_ID + '/bridge.js'
   var test_file = 'http://update.predictablefarm.net/7/bridge.js' // debug
@@ -104,7 +182,7 @@ var networkCheck = setInterval(function () {
   })
 }, 60 * 1000)
 
-logger.log(' 4. Starting reboot check')
+logger.log(' 5. Starting reboot check')
 var rebootCheck = setInterval(function () {
   if (fs.existsSync('/root/reboot.lock')) {
     networkStatus.remote_socket = false
@@ -114,7 +192,7 @@ var rebootCheck = setInterval(function () {
   }
 }, 20 * 1000)
 
-logger.log(' 5. run')
+logger.log(' 6. Run')
 
 /*
         UTILITY FUNCTIONS
@@ -122,16 +200,16 @@ logger.log(' 5. run')
 var ip = '127.0.0.1'
 
 function getIpAddr () {
-  var cmd = 'ifconfig eth1 | grep "inet addr" | cut -d ":" -f 2 | cut -d " " -f 1'
+  var cmd = 'ifconfig ' + config.interface + ' | grep "inet addr" | cut -d ":" -f 2 | cut -d " " -f 1'
   exec(cmd, function (error, stdout, stderr) {
     if (stdout.length > 10) {
-      ip = stdout
+      ip = stdout.trim()
     }
-    logger.log('IP : %s', ip)
+    // logger.log('IP : %s', ip)
     var msg2send = JSON.stringify({
       device_id: DEVICE_ID.toString(),
       sensor_type: 'ip',
-      sensor_value: ip
+      sensor_value: ip + '(' + config.interface + ')'
     })
     print_screen(msg2send)
   })
@@ -143,6 +221,12 @@ function all_relay_off () {
 }
 
 function exitProcess () {
+  // Cleaner exit ?
+  remote_socket.disconnect(0)
+  for (var socket in local_sockets) {
+    socket.disconnect(0)
+  }
+
   utils.led(utils.colors.RED)
   logger.log('** Exit process now')
   networkStatus.network = false
@@ -216,11 +300,16 @@ function isRelay (command) {
 }
 
 function processIncomingCommand (data, mode) {
-  var prefix = (mode == MODE_LOCAL) ? 'LOCAL Command' : 'REMOTE Command'
+  if (config.local.ignore_when_connected && (mode == MODE_LOCAL) && networkStatus.remote_socket) {
+    // When connected to the Internet, ignore local commands
+    return
+  }
+
+  var prefix = (mode == MODE_LOCAL) ? 'LOCAL' : 'REMOTE'
 
   logger.log(prefix + ' : ' + data)
 
-  d = data.replace(/\\/g, '')
+  var d = data.replace(/\\/g, '')
 
   // remove first " and last " from a dirty string if they exist
   if ((d.charAt(0) == '"') && (d.slice(-1) == '"')) {
@@ -245,6 +334,36 @@ function processIncomingCommand (data, mode) {
 /*
         SOCKET EVENTS
 */
+
+remote_socket.on('local-graph', function (data) {
+  logger.log('Received new local graph from remote server')
+  // Remove previous file
+  fs.unlink(GRAPH_FILE, function (err) {
+    // If the file does not exist, it's ok
+    if (err && err.code != 'ENOENT') {
+      return
+    }
+
+    // Store new graph and replace "graph" variable in-memory
+    try {
+      fs.writeFileSync(GRAPH_FILE, JSON.stringify(data))
+      graph = data
+    } catch (err) {
+      return
+    }
+
+    // Emit set-config again
+    remote_socket.emit('local-graph', 'Local graph successfully saved.')
+
+    if (engine_socket) {
+      logger.log('** Re-emitting set-config to local engine')
+      engine_socket.emit('set-config')
+    } else {
+      logger.log('** Relaunching process to create the engine socket')
+      exitProcess()
+    }
+  })
+})
 
 remote_socket.on('connect', function () {
   logger.log('** Connected to server: ' + SERVER_URL)
@@ -389,7 +508,20 @@ my_io.on('connection', function (my_io_socket) {
     logger.log(' --> Sending to server: ' + data)
 
     utils.led(utils.colors.GREEN)
-    remote_socket.emit('sensor-emit', data)
+
+    if (networkStatus.remote_socket) {
+      // logger.log(" --> Sending to remote server: " + data);
+      remote_socket.emit('sensor-emit', data)
+    }
+
+    // Do we also need to send the sensor message to local sockets ?
+    if ((!data.sensor_type || data.sensor_type.slice(0, 5) != 'relay') && local_sockets.length > 0) {
+      // logger.log(" --> Sending to local sockets: " + data);
+      local_sockets.forEach(function (socket) {
+        socket.emit('sensor-emit', data)
+      })
+    }
+
     print_screen(data)
   })
 
