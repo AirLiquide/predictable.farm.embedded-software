00f5593efec20da02f18a1c3ef13de5bd8bbfe22
tchapi <regbasket+github@gmail.com>
2018-04-11 08:15:28 +0200

    Move watchdog to scripts/

---
+++ iot2000/scripts/wd_check_i2c.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# while true
+# do
+if mraa-i2c get 0 0x27 0 | grep "Register 00 = 00" > /dev/null
+then
+  echo "i2c ok"
+  exit 0
+else
+  echo "i2c ko"
+  exit 255 #reboot
+fi
+# done
