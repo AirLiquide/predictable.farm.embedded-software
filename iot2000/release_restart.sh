386628c160c5930aabd7f8ea69b3fadc375615aa
tchapi <regbasket+github@gmail.com>
2018-05-07 14:38:35 +0200

    Fix restart script

--- iot2000/release_restart.sh
+++ iot2000/release_restart.sh
@@ -4,11 +4,9 @@ WORKDIR=/home/root/predictable-farm
 echo ""
 echo "----------"
 echo "POSTINSTALL : chmod +x"
-chmod +x $WORKDIR/*
-
-echo "POSTINSTALL : remove update package from target"
-rm $WORKDIR/*.swu
+chmod +x $WORKDIR/*.sh
+chmod +x $WORKDIR/scripts/*.sh
 
 echo "POSTINSTALL : Restarting forever"
-forever stopall
-cd $WORKDIR && forever start forever.json
+/usr/lib/node_modules/forever/bin/forever stopall
+cd $WORKDIR && /usr/lib/node_modules/forever/bin/forever start forever.json
